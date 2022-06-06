c43098030218050339649a88de7e93b66fe017da
VladyslavOlegovich <vladionkin@icloud.com>
2022-06-06 18:24:18 +0300

    added secret button

---
+++ index.js
@@ -0,0 +1,14 @@
+// let button = document.querySelector('.btn');
+// let paragraph = document.querySelector('.skill');
+
+function showInfo() {
+    document.getElementById('skill').innerHTML = 'my most important skill is Curiosity';
+    // paragraph.innerHTML = 'my most important skill is Curiosity'
+
+}
+
+
+function clearInfo() {
+    paragraph.innerHTML = '';
+}
+setTimeout(clearInfo, 5000)
